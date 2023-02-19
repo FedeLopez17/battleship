724f832279f7360e99571e0a89148a79c630de86
FedeLopez17 <fedelopez1712@gmail.com>
2023-02-19 00:08:38 -0300

    Add arrIncludesObj helper function

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,3 +1,8 @@
 export default function randomIntegerInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function arrIncludesObj(arr, obj) {
+  const objKeys = Object.keys(obj);
+  return arr.some((arrObj) => objKeys.every((key) => arrObj[key] === obj[key]));
+}
